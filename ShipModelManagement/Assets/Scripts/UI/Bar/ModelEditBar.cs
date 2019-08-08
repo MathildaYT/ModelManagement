c6278f67f6dd627629678fa3207e550072603e25
DESKTOP-ICH1PC8\Luke <zhangf@lingest.com>
2019-08-08 18:25:03 +0800

    修改viewscene 上传一些modeleditbar逻辑

--- ShipModelManagement/Assets/Scripts/UI/Bar/ModelEditBar.cs
+++ ShipModelManagement/Assets/Scripts/UI/Bar/ModelEditBar.cs
@@ -1,12 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ModelEditBar : UIBar
 {
+    Dropdown _selectpanels;
+
     public override void OnOpen()
     {
         base.OnOpen();
+
+        var panels = new List<string>();
+        panels.Add("模型编辑");
+        panels.Add("模型查看");
+        panels.Add("模型录入");
+
+        _selectpanels.AddOptions(panels);
     }
 
     public override void OnClose()
