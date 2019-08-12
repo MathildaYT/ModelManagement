fa9b99fa94cd59563a9501f833d3af23d3b6e025
DESKTOP-ICH1PC8\Luke <zhangf@lingest.com>
2019-08-12 10:34:51 +0800

    增加monohelper

---
+++ ShipModelManagement/Assets/Scripts/Common/MonoHelper.cs
@@ -0,0 +1,136 @@
+﻿#region 描述
+//-----------------------------------------------------------------------------
+// 类 名 称: MonoHelper
+// 作    者：zhangfan
+// 创建时间：2019/8/12 10:28:59
+// 描    述：
+// 版    本：
+//-----------------------------------------------------------------------------
+// Copyright (C) 2017-2019 零境科技有限公司
+//-----------------------------------------------------------------------------
+#endregion
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public delegate void MonoUpdaterEvent();
+
+public class MonoHelper : MonoSingleton<MonoHelper>
+{
+    //===========================================================
+
+    private event MonoUpdaterEvent UpdateEvent;
+    private event MonoUpdaterEvent FixedUpdateEvent;
+    private event MonoUpdaterEvent LateUpdateEvent;
+
+    public static void AddUpdateListener(MonoUpdaterEvent listener)
+    {
+        if (Instance != null)
+        {
+            Instance.UpdateEvent += listener;
+        }
+    }
+
+    public static void RemoveUpdateListener(MonoUpdaterEvent listener)
+    {
+        if (Instance != null)
+        {
+            Instance.UpdateEvent -= listener;
+        }
+    }
+
+    public static void AddFixedUpdateListener(MonoUpdaterEvent listener)
+    {
+        if (Instance != null)
+        {
+            Instance.FixedUpdateEvent += listener;
+        }
+    }
+
+    public static void RemoveFixedUpdateListener(MonoUpdaterEvent listener)
+    {
+        if (Instance != null)
+        {
+            Instance.FixedUpdateEvent -= listener;
+        }
+    }
+
+    public static void AddLateUpdateListener(MonoUpdaterEvent listener)
+    {
+        if (Instance != null)
+        {
+            Instance.LateUpdateEvent += listener;
+        }
+    }
+
+    public static void RemoveLateUpdateListener(MonoUpdaterEvent listener)
+    {
+        if (Instance != null)
+        {
+            Instance.LateUpdateEvent -= listener;
+        }
+    }
+
+    void Update()
+    {
+        if (UpdateEvent != null)
+        {
+            try
+            {
+                UpdateEvent();
+            }
+            catch (Exception e)
+            {
+                Debug.LogErrorFormat("Update() Error:{0}\n{1}", e.Message, e.StackTrace);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (FixedUpdateEvent != null)
+        {
+            try
+            {
+                FixedUpdateEvent();
+            }
+            catch (Exception e)
+            {
+                Debug.LogErrorFormat("FixedUpdate() Error:{0}\n{1}", e.Message, e.StackTrace);
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (LateUpdateEvent != null)
+        {
+            try
+            {
+                LateUpdateEvent();
+            }
+            catch (Exception e)
+            {
+                Debug.LogErrorFormat("LateUpdateEvent() Error:{0}\n{1}", e.Message, e.StackTrace);
+            }
+        }
+    }
+
+    //===========================================================
+
+    public new static void StartCoroutine(IEnumerator routine)
+    {
+        MonoBehaviour mono = Instance;
+        mono.StartCoroutine(routine);
+    }
+
+    public new static void StopCoroutine(IEnumerator routine)
+    {
+        MonoBehaviour mono = Instance;
+        mono.StopCoroutine(routine);
+    }
+}
---
+++ ShipModelManagement/Assets/Scripts/Common/MonoSingleton.cs
@@ -0,0 +1,77 @@
+﻿#region 描述
+//-----------------------------------------------------------------------------
+// 类 名 称: MonoSingleton
+// 作    者：zhangfan
+// 创建时间：2019/8/12 10:29:39
+// 描    述：
+// 版    本：
+//-----------------------------------------------------------------------------
+// Copyright (C) 2017-2019 零境科技有限公司
+//-----------------------------------------------------------------------------
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+{
+    private static T ms_instance;
+    public static T Instance
+    {
+        get
+        {
+            if (ms_instance == null)
+            {
+                ms_instance = Instantiate();
+            }
+
+            return ms_instance;
+        }
+    }
+
+    protected static T Instantiate()
+    {
+        if (ms_instance == null)
+        {
+            ms_instance = (T)FindObjectOfType(typeof(T));
+            if (FindObjectsOfType(typeof(T)).Length > 1)
+            {
+                return ms_instance;
+            }
+
+            if (ms_instance == null)
+            {
+                GameObject singleton = new GameObject("[Singleton]" + typeof(T).Name);
+                if (singleton != null)
+                {
+                    ms_instance = singleton.AddComponent<T>();
+                    ms_instance.InitSingleton();
+                }
+
+            }
+        }
+
+        return ms_instance;
+    }
+
+    protected virtual void InitSingleton()
+    {
+
+    }
+
+    private void Awake()
+    {
+        if (ms_instance == null)
+        {
+            ms_instance = this as T;
+        }
+    }
+
+    public void OnApplicationQuit()
+    {
+        ms_instance = null;
+    }
+}
